2760abd60e74525ea048821213db6f91a68506db
Endymion Cheung <endymion.cheung@gmail.com>
2019-10-24 23:10:48 -0400

    Moved video.py to video.ipynb for editing
    Moved the video.py to the Jupyter notebook for easier editing

--- video.py
+++ video.py
@@ -1,18 +1,40 @@
-from moviepy.editor import ImageSequenceClip
-import argparse
-import os
 
+# coding: utf-8
+
+# In[1]:
+
+
+# video.py
+'''
+The "video" script takes the list of the generated images in sequence and creates a video
+'''
+
+# File system and command line options library
+import os
+import argparse
+
+# Video clip editor library
+from moviepy.editor import ImageSequenceClip
+
+# Supported image extensions
 IMAGE_EXT = ['jpeg', 'gif', 'png', 'jpg']
+
+
+# In[ ]:
 
 
 def main():
     parser = argparse.ArgumentParser(description='Create driving video.')
+    
+    # Input argument for the folder containing source images
     parser.add_argument(
         'image_folder',
         type=str,
         default='',
         help='Path to image folder. The video will be created from these images.'
     )
+    
+    # Input argument for the frame rate (FPS) default to 60FPS if not specified
     parser.add_argument(
         '--fps',
         type=int,
@@ -20,17 +42,18 @@ def main():
         help='FPS (Frames per second) setting for the video.')
     args = parser.parse_args()
 
-    #convert file folder into list firltered for image file types
+    # Convert file folder into list filtered for image file types
     image_list = sorted([os.path.join(args.image_folder, image_file)
                         for image_file in os.listdir(args.image_folder)])
     
     image_list = [image_file for image_file in image_list if os.path.splitext(image_file)[1][1:].lower() in IMAGE_EXT]
 
-    #two methods of naming output video to handle varying environemnts
+    # Two methods of naming output video to handle varying environemnts
     video_file_1 = args.image_folder + '.mp4'
     video_file_2 = args.image_folder + 'output_video.mp4'
 
-    print("Creating video {}, FPS={}".format(args.image_folder, args.fps))
+    # Create the video clip from the sequence of images
+    print("Creating video {} @FPS={} ...".format(args.image_folder, args.fps))
     clip = ImageSequenceClip(image_list, fps=args.fps)
     
     try:
@@ -41,3 +64,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
